9ce5ec95c3265806aabc56343658257d467c62eb
Mathias <mathias.luecker@gmail.com>
2013-06-16 13:09:25 +0200

    Node.js hinzugefügt

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  app = express();
+
+// Configuration
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/app'));
+});
+
+app.configure('development', function () {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.set('port', 9292);
+});
+
+var server = app.listen(app.settings.port);
+console.log("Express server is listening on http://%s:%s in '%s' mode", server.address().address, server.address().port, app.settings.env);
