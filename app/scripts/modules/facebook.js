915ada35a525edd8faa0a46efcc8e71e451c1e39
Sascha <haveagoodone76@googlemail.com>
2013-07-28 00:15:06 +0200

    Calculate the distance from each place to the current location and sort the list with this order

--- app/scripts/modules/facebook.js
+++ app/scripts/modules/facebook.js
@@ -9,6 +9,7 @@ define([
   Facebook.Model = Backbone.Model.extend({ });
 
   Facebook.Collection = Backbone.Collection.extend({
+
     initialize: function(models, options){
       this.lat = options.lat;
       this.lon = options.lon;
@@ -22,7 +23,52 @@ define([
     },
     
     parse : function(response){
+
+      var point1 = {
+        lat: parseFloat(this.lat),
+        lon: parseFloat(this.lon)
+      }
+
+      _.each(response.data, function(place) {
+
+        var point2 = {
+          lat: parseFloat(place.location.latitude),
+          lon: parseFloat(place.location.longitude)
+        }        
+
+        place.distance = parseInt(this.calculateDistance(point1, point2) * 1000);
+      }, this);
+
       return response.data;
+    },
+
+    comparator: function(item) {
+      return -item.get("distance");
+    },
+
+    calculateDistance: function(point1, point2) {
+      var point1_lat = point1.lat;
+      var point1_lon = point1.lon;
+      var point2_lat = point2.lat;
+      var point2_lon = point2.lon;
+
+      var R = 6371; // km
+      var dLat = this.toRad(point2_lat - point1_lat);
+      var dLon = this.toRad(point2_lon - point1_lon);
+      var lat1 = this.toRad(point1_lat);
+      var lat2 = this.toRad(point2_lat);
+
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var distance = R * c;
+
+      return distance;
+    },
+
+    toRad: function(Value) {
+      /** Converts numeric degrees to radians */
+      return Value * Math.PI / 180;
     },
     
     fetch: function(options) {
@@ -89,7 +135,9 @@ define([
       $('#resultHead').html('Ergebnisse ('+this.collection.length+')');
       
       this.collection.each(function(post){
-        var ItemView=new Facebook.ItemView({model: post});
+        var ItemView = new Facebook.ItemView({
+          model: post
+        });
         $('.post-list').prepend(ItemView.render().el);
       });
 
