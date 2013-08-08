2938b5921542ffdb0a87ea6ccadfacf93e591ab6
Sascha <haveagoodone76@googlemail.com>
2013-08-08 23:12:54 +0200

    Add a fallback. If there are no places to zoom to, zoom to the browser-location

--- app/scripts/modules/map.js
+++ app/scripts/modules/map.js
@@ -20,83 +20,107 @@ function(Backbone, Config, Template, Ol) {
 
     initialize: function() {
       this.model = new Map.Model();
+
+      this.initializeMap();
  
-      this.model.on('change:position', this.setCenterPosition, this);
-
+      // get the places from the facebook-Module
+      // ToDo: If there a now playes, who will center the map?
       this.listenTo(Backbone, 'setPlacesToMap', this.addPlacesToMap);
-
+      
+      // the current location of the browser is located
+      this.model.on('change:homePosition', this.setHomeMarker, this);
+      
+      // highlight a feature in the map, if the user clicks on a list-item
       this.listenTo(Backbone, 'highlightFeature', this.highlightFeature);
+      
+      // after setting a new Location, set a new Location
+      this.listenTo(Backbone, 'setNewLocation', this.setNewLocation); 
 
-      this.listenTo(Backbone, 'setNewLocation', this.setMapCenter);
+      // ToDo: use the openlayers getLocation-Functionality
+      this.getCenterPosition();
+    },
 
-      this.cloudmadeLayer = new Map.CloudMade("CloudMade", {
-        key: Config.cloudmade.apiKey,
-        styleId: 96931
+    render: function() {
+      this.$el.empty().append(this.template(this.model));
 
+      this.map.render('map-container');
+
+      return this;
+    },
+
+    initializeMap: function() {
+      /*
+       * Create Styles for the homeLayer and featureLayer
+       */
+      var defaultFeatureStyle = new OpenLayers.Style({
+        'externalGraphic' : 'img/marker.png',
+        'graphicWidth'    : 28,
+        'graphicHeight'   : 28,
+        'graphicYOffset'  : -24,
+        'title'           : '${tooltip}'
       });
 
-      this.map = new OpenLayers.Map({
-        controls: [
-          new OpenLayers.Control.Navigation(),
-          new OpenLayers.Control.Zoom()
-        ],
-        layers: [
-          this.cloudmadeLayer
-          // new OpenLayers.Layer.OSM("OpenStreetMap", null, {
-          //   transitionEffect: "resize"
-          // })
-        ]
-      });
-
-      var defaultStyle = new OpenLayers.Style({
-        'externalGraphic': 'img/marker.png',
-        'graphicWidth' : 28,
-        'graphicHeight' : 28,
-        'graphicYOffset' : -24,
-        'title' : '${tooltip}'
-      });
-
-      var selectStyle = new OpenLayers.Style({
-        'externalGraphic': 'img/marker_selected.png',
+      var selectedFeatureStyle = new OpenLayers.Style({
+        'externalGraphic' : 'img/marker_selected.png',
         'graphicWidth'    : 28,
         'graphicHeight'   : 28,
         'graphicYOffset'  : -24,
         'title'           : '${tooltip}'
       }); 
 
-      var homeStyle = new OpenLayers.Style({
-        'externalGraphic': 'img/home.png',
+      var homeLayersStyle = new OpenLayers.Style({
+        'externalGraphic' : 'img/home.png',
         'graphicWidth'    : 36,
         'graphicHeight'   : 36,
         'graphicYOffset'  : -24,
         'title'           : '${tooltip}'
       });
 
+      /*
+       * Initialize Layers
+       *
+       * homeLayer      --> Layer for the home marker
+       * featureLayer   --> Layer for the facebook-features
+       * cloudmadeLayer --> Background-Layer
+       */
+      this.cloudmadeLayer = new Map.CloudMade("CloudMade", {
+        key: Config.cloudmade.apiKey,
+        styleId: 96931
+      });
+
       this.homeLayer = new OpenLayers.Layer.Vector('HomeLayer', {
         styleMap: new OpenLayers.StyleMap({
-          'default': homeStyle
+          'default': homeLayersStyle
         })
       });
 
-      this.overlay = new OpenLayers.Layer.Vector('Overlay', {
+      this.featureLayer = new OpenLayers.Layer.Vector('featureLayer', {
         styleMap: new OpenLayers.StyleMap({
-          'default': defaultStyle,
-          'select': selectStyle
+          'default': defaultFeatureStyle,
+          'select': selectedFeatureStyle
         })
       });
 
-      this.overlay.events.on({
-          'featureselected': function(feature) {
-            Backbone.trigger('clickOnMapMarker', feature.feature.attributes.id);
-          },
-          'featureunselected': function(feature) {
-              // console.log("Unselected");
-          }
+      /*
+       * Create a new Map
+       */
+      this.map = new OpenLayers.Map({
+        controls: [
+          new OpenLayers.Control.Navigation(),
+          new OpenLayers.Control.Zoom()
+        ],
+        layers: [
+          this.cloudmadeLayer,
+          this.featureLayer,
+          this.homeLayer
+        ]
       });
 
-      // Add a control for selecting a marker
+      /*
+       * Add a control for selecting a marker
+       */
       this.selectControl = new OpenLayers.Control.SelectFeature(
-        this.overlay,
+        this.featureLayer,
         {
           box: false,
           hover: false,
@@ -106,116 +130,117 @@ function(Backbone, Config, Template, Ol) {
           toggleKey: "ctrlKey", // ctrl key removes from selection
           multipleKey: "shiftKey" // shift key adds to selection
         }
-      )
-                
-      this.map.addLayer(this.overlay);
-
-      this.map.addLayer(this.homeLayer);
-
+      );
+      
+      /*
+       * Add and activate the select control
+       */
       this.map.addControl(this.selectControl);
-
-      this.getCenterPosition();
+      this.selectControl.activate();
+      
+      /*
+       * Events for the featureLayer-Layer
+       */
+      this.featureLayer.events.on({
+        'featureselected': function(feature) {
+          Backbone.trigger('clickOnMapMarker', feature.feature.attributes.id);
+        },
+        'featureunselected': function(feature) {
+          // ToDo: Unselect the feature
+        }
+      });
     },
 
-    setCenterPosition: function(position) {
-      this.render();
+    addPlacesToMap: function(models) {
+      // reset the featureLayer
+      this.featureLayer.destroyFeatures();
+
+      if (models.length > 0) {
+        // Add markers to the map
+        _.each(models, function(model) {
+          var latitude = model.get('location').latitude;
+          var longitude = model.get('location').longitude;
+          var locationName = model.get('name');
+          var myLocation = new OpenLayers.Geometry.Point(longitude, latitude).transform('EPSG:4326', 'EPSG:3857');
+          
+          this.featureLayer.addFeatures([
+            new OpenLayers.Feature.Vector(myLocation, {
+              id: locationName,
+              tooltip: locationName
+            })
+          ]);
+        }, this);
+
+        this.map.zoomToExtent(this.featureLayer.getDataExtent());
+
+      } else {
+
+        var position = {
+          lat: this.model.get('homePosition').lat,
+          lon: this.model.get('homePosition').lon,
+          reCenter: true
+        }
+        this.setHomeMarker(position);
+      }
     },
 
     getCenterPosition: function() {
       var self = this;
+
       navigator.geolocation.getCurrentPosition( function(currentPosition) {
-        self.model.set({ position: currentPosition });
+        var newLocation = {
+          lat: currentPosition.coords.latitude,
+          lon: currentPosition.coords.longitude,
+          reCenter: true
+        }
+        self.model.set({ homePosition: newLocation });
       });
     },
 
-    render: function() {
-      this.$el.empty().append(this.template(this.model));
-
-      if (this.model.get('position')) {
-        this.renderMap();
+    setNewLocation: function(model) {
+      var newLocation = {
+        lat: model.lat,
+        lon: model.lon,
+        reCenter: false
       }
-
-      return this;
+      this.model.set({ homePosition: newLocation });
     },
 
-    renderMap: function(position) {
-      var latitude = this.model.get('position').coords.latitude;
-      var longitude = this.model.get('position').coords.longitude;
-
-      this.map.render('map-container');
-
-      this.map.setCenter(new OpenLayers.LonLat(longitude,latitude)
-        .transform(
-          new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-          new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
-        ), 
-        15 // Zoom level
-      );
-
-      this.setHomeMarker(latitude, longitude);
-    },
-
-    setHomeMarker: function(lat, lon) {
-
+    setHomeMarker: function(position) {
+      var lat = position.lat || position.get('homePosition').lat;
+      var lon = position.lon || position.get('homePosition').lon;
       var homeLocation = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:3857');
       
       this.homeLayer.destroyFeatures();
 
       this.homeLayer.addFeatures([
         new OpenLayers.Feature.Vector(homeLocation, {
-          tooltip: 'Home',
-          id: 'home'
+          id: 'home',
+          tooltip: 'Home'
         })
       ]);
-    },
 
-    setMapCenter: function(newCenter) {
-      var latitude = newCenter.lat;
-      var longitude = newCenter.lon;
-
-      this.map.setCenter(new OpenLayers.LonLat(longitude,latitude)
+      if (position.reCenter) {
+        this.map.setCenter(new OpenLayers.LonLat(lon,lat)
         .transform(
-          new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-          new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
+          new OpenLayers.Projection("EPSG:4326"),
+          new OpenLayers.Projection("EPSG:900913")
         ), 
-        15 // Zoom level
+        13
       );
-
-      this.setHomeMarker(latitude, longitude);
+      }
     },
 
     highlightFeature: function(model){
-
       this.selectControl.unselectAll();
 
-      var even = _.find(this.overlay.features, function(feature,index){
+      var even = _.find(this.featureLayer.features, function(feature,index){
         return feature.attributes.id == model.attributes.name;
       });
       
       this.selectControl.select(even);
-    },
-
-    addPlacesToMap: function(models) {
-
-      this.selectControl.activate();
-
-      this.overlay.destroyFeatures();
-
-      // Add markers to the map
-      _.each(models, function(model) {
-        var latitude = model.get('location').latitude;
-        var longitude = model.get('location').longitude;
-        var locationName = model.get('name');
-        var myLocation = new OpenLayers.Geometry.Point(longitude, latitude).transform('EPSG:4326', 'EPSG:3857');
-        this.overlay.addFeatures([
-          new OpenLayers.Feature.Vector(myLocation, {
-            tooltip: locationName,
-            id: locationName
-          })
-        ]);
-      }, this);
-      this.map.zoomToExtent(this.overlay.getDataExtent()); 
     }
+
   });
 
   Map.CloudMade = OpenLayers.Class(OpenLayers.Layer.XYZ, {
